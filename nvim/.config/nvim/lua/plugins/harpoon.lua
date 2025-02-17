b393c558bf612d978ba157bba9f1b4aaaa6bb3ed
daidv94 <dai.dao@noreply.github.com>
2025-02-17 10:57:36 +0700

    chore: remove comments, useless code
    Signed-off-by: daidv94 <dai.dao@noreply.github.com>

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -6,30 +6,8 @@ return {
     require("harpoon").setup()
     local harpoon = require("harpoon")
 
-    -- basic telescope configuration
-    -- local conf = require("telescope.config").values
-    -- local function toggle_telescope(harpoon_files)
-    --   local file_paths = {}
-    --   for _, item in ipairs(harpoon_files.items) do
-    --     table.insert(file_paths, item.value)
-    --   end
-    --
-    --   require("telescope.pickers")
-    --       .new({}, {
-    --         prompt_title = "Harpoon",
-    --         finder = require("telescope.finders").new_table({
-    --           results = file_paths,
-    --         }),
-    --         previewer = conf.file_previewer({}),
-    --         sorter = conf.generic_sorter({}),
-    --       })
-    --       :find()
-    -- end
-
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    -- vim.keymap.set("n", "<leader>ht", function() toggle_telescope(harpoon:list()) end,
-    --   { desc = "Open harpoon window by telescope" })
 
     vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end)
     vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end)
