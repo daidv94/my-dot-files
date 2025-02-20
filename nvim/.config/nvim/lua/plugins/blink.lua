d43ea7cde4b175a56e4b0b1c974e39253789fde1
daidv94 <dai.dao@noreply.github.io>
2025-02-20 21:47:59 +0700

    chore: change min key word to trigger cmp
    Signed-off-by: daidv94 <dai.dao@noreply.github.io>

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -87,7 +87,7 @@ return {
       default = { "lsp", "path", "snippets", "buffer" },
       providers = {
         lsp = {
-          min_keyword_length = 2, -- Number of characters to trigger provider
+          min_keyword_length = 1, -- Number of characters to trigger provider
           score_offset = 0, -- Boost/penalize the score of the items
         },
         path = {
