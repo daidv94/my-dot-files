c4953adade9eb082e9abfa85b96c4745cdfb7656
daidv94 <dai.dao@noreply.github.com>
2025-07-03 21:43:25 +0700

    feat: add copilot
    Signed-off-by: daidv94 <dai.dao@noreply.github.com>

--- nvim/.config/nvim/lua/plugins/gitsign.lua
+++ nvim/.config/nvim/lua/plugins/gitsign.lua
@@ -2,6 +2,7 @@ return {
   "lewis6991/gitsigns.nvim",
   version = "*",
   lazy = false,
+  -- adf
   dependencies = { "sindrets/diffview.nvim" },
   config = function()
     require("gitsigns").setup()
