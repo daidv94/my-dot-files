39d8a979f6c27a857a8adfd9c030952ca7eeea75
daidv94 <dai.dao@noreply.github.io>
2025-08-03 10:16:28 +0700

    feat: upgrade treesitter
    Signed-off-by: daidv94 <dai.dao@noreply.github.io>

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    -- "nvim-treesitter/nvim-treesitter-context", # Enable if I become a software engineer
+  },
   config = function()
     require("nvim-treesitter.configs").setup({
       -- A list of parser names, or "all"
@@ -22,12 +26,17 @@ return {
         "terraform",
         "dockerfile",
       },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gsi",
+          node_incremental = "gsn",
+          scope_incremental = "gsc",
+          node_decremental = "gsd",
+        },
+      },
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
       auto_install = true,
 
       indent = {
@@ -35,14 +44,40 @@ return {
       },
 
       highlight = {
-        -- `false` will disable the whole extension
         enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+
+          lookahead = true,
+
+          keymaps = {
+            ["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
+            ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
+            ["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            ["ab"] = { query = "@block.outer", desc = "Select outer part of a codeblock" },
+            ["ib"] = { query = "@block.inner", desc = "Select outer part of a codeblock" },
+            ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+            ["il"] = { query = "@loop.inner", desc = "Select outer part of a loop" },
+
+            ["iq"] = "@string.inner",
+            ["aq"] = "@string.outer",
+
+            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+          },
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
+          },
+          -- If you set this to `true` (default is `false`) then any textobject is
+          -- extended to include preceding or succeeding whitespace. Succeeding
+          -- whitespace has priority in order to act similarly to eg the built-in
+          include_surrounding_whitespace = true,
+        },
       },
     })
 
