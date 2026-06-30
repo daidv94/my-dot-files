bc39f4447294c08a51428ea30a1307e992b300c9
daidv94 <daidv94@github.noreply.io>
2026-06-30 20:57:20 +0700

    feat: update wezterm config
    Signed-off-by: daidv94 <daidv94@github.noreply.io>

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -25,10 +25,10 @@ return {
             },
           })
         end,
-      }, -- or zbirenbaum/copilot.lua
+      },                                              -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
+    build = "make tiktoken",                          -- Only on MacOS or Linux
     opts = {
       prompts = {
         Rename = {
@@ -41,15 +41,15 @@ return {
       },
     },
     keys = {
-      { "<leader>cc", ":CopilotChat<CR>", mode = "n", desc = "Chat with Copilot" },
-      { "<leader>ce", ":CopilotChatExplain<CR>", mode = "v", desc = "Explain Code" },
-      { "<leader>cr", ":CopilotChatReview<CR>", mode = "v", desc = "Review Code" },
-      { "<leader>cf", ":CopilotChatFix<CR>", mode = "v", desc = "Fix Code Issues" },
+      { "<leader>cc", ":CopilotChat<CR>",         mode = "n", desc = "Chat with Copilot" },
+      { "<leader>ce", ":CopilotChatExplain<CR>",  mode = "v", desc = "Explain Code" },
+      { "<leader>cr", ":CopilotChatReview<CR>",   mode = "v", desc = "Review Code" },
+      { "<leader>cf", ":CopilotChatFix<CR>",      mode = "v", desc = "Fix Code Issues" },
       { "<leader>co", ":CopilotChatOptimize<CR>", mode = "v", desc = "Optimize Code" },
-      { "<leader>cd", ":CopilotChatDocs<CR>", mode = "v", desc = "Generate Docs" },
-      { "<leader>ct", ":CopilotChatTests<CR>", mode = "v", desc = "Generate Tests" },
-      { "<leader>cm", ":CopilotChatCommit<CR>", mode = "n", desc = "Generate Commit Message" },
-      { "<leader>cs", ":CopilotChatCommit<CR>", mode = "v", desc = "Generate Commit for Selection" },
+      { "<leader>cd", ":CopilotChatDocs<CR>",     mode = "v", desc = "Generate Docs" },
+      { "<leader>ct", ":CopilotChatTests<CR>",    mode = "v", desc = "Generate Tests" },
+      { "<leader>cm", ":CopilotChatCommit<CR>",   mode = "n", desc = "Generate Commit Message" },
+      { "<leader>cs", ":CopilotChatCommit<CR>",   mode = "v", desc = "Generate Commit for Selection" },
     },
   },
 }
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -38,7 +38,8 @@ config.colors = {
   selection_fg = "#e0def4", -- rose-pine text
   selection_bg = "#403d52", -- rose-pine highlight_med
 }
-config.window_background_opacity = 1
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
 -- config.window_decorations = "RESIZE"
 config.default_workspace = "main"
 
